2b11d42d7c386fd88b682d68702c8bf9a5e1e6ad
Arthur Taft <arthur.taft2006@icloud.com>
2025-10-12 20:58:17 -0600

    Add functions to install and uninstall packages

--- linux-hardening/first30.sh
+++ linux-hardening/first30.sh
@@ -223,3 +223,95 @@ function check_package_manager() {
 }
 
 package_manager=$(check_package_manager)
+
+function remove_package() {
+    local package_manager="$1"
+    if [[ -z "$package_manager" ]]; then
+        echo "No package manager provided to remove package :("
+        exit 1
+    fi
+
+    local package_name="$2"
+    if [[ -z "$package_name" ]]; then
+        echo "No package name provided to remove package :("
+        exit 1
+    fi
+
+    if [[ "$package_manager" == "unsupported" ]]; then
+        echo "Unsupported operating system :("
+        exit 1
+    fi
+
+    case "$package_manager" in
+        apt)
+            echo "Using apt to remove $package_name..."
+            apt remove -y "$package_name"
+            ;;
+        dnf)
+            echo "Using dnf to remove $package_name..."
+            dnf remove -y "$package_name"
+            ;;
+        yum)
+            echo "Using yum to remove $package_name..."
+            yum remove -y "$package_name"
+            ;;
+        pacman)
+            echo "Using pacman to remove $package_name..."
+            pacman -R --noconfirm "$package_name"
+            ;;
+        zypper)
+            echo "Using zypper to remove $package_name..."
+            zypper remove -y "$package_name"
+            ;;
+        *)
+            echo "Unsupported package manager :("
+            exit 1
+            ;;
+    esac
+}
+
+function install_package() {
+    local package_manager="$1"
+    if [[ -z "$package_manager" ]]; then
+        echo "Error: No package manager provided to install package :("
+        exit 1
+    fi
+
+    local package_name="$2"
+    if [[ -z "$package_name" ]]; then
+        echo "Error: No package name provided to install package :("
+        exit 1
+    fi
+
+    if [[ "$package_manager" == "unsupported" ]]; then
+        echo "Error: Unsupported operating system :("
+        exit 1
+    fi
+
+    case "$package_manager" in
+        apt)
+            echo "Using apt to install $package_name..."
+            apt update && apt install -y "$package_name"
+            ;;
+        dnf)
+            echo "Using dnf to install $package_name..."
+            dnf install -y "$package_name"
+            ;;
+        yum)
+            echo "Using yum to install $package_name..."
+            yum install -y "$package_name"
+            ;;
+        pacman)
+            echo "Using pacman to install $package_name..."
+            pacman -Syu --noconfirm "$package_name"
+            ;;
+        zypper)
+            echo "Using zypper to install $package_name..."
+            zypper install -y "$package_name"
+            ;;
+        *)
+            echo "Unsupported package manager :("
+            exit 1
+            ;;
+    esac
+}
