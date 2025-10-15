6770b916dcae54fc56aa6a148254fe96587da3aa
Arthur Taft <arthur.taft2006@icloud.com>
2025-10-14 19:16:38 -0600

    Add logic to wait for users to be updated before backing things up again

--- linux-hardening/first30.sh
+++ linux-hardening/first30.sh
@@ -221,7 +221,8 @@ function setup_tmux() {
       sleep 0.3
       tmux send-keys -t start:0.0 'bash -c "update_user_pass"' C-m
       tmux send-keys -t start:0.1 "bash -c 'service_backup && interface_down $interfaces $modify_iface'" C-m
-      tmux send-keys -t start:0.0 'bash -c "create_backup_usr && second_pass_update"' C-m
+      tmux send-keys -t start:0.0 'bash -c "create_backup_usr && second_pass_update; tmux wait-for -S usr_ready"' C-m
+      tmux wait-for usr_ready
       tmux send-keys -t start:0.1 'bash -c "nuke_cron && backup_group_passwd_shadow && backup_etc && interface_up interfaces modify_iface"' C-m
       tmux send-keys -t start:1.0 'vim /etc/ssh/sshd_config' C-m
       tmux send-keys -t start:1.1 'vim /etc/issue.net' C-m
